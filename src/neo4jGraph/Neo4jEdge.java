8c74d08be2aba07961cc5b199524e2eb5d1c15cf
iz2 <devnull@localhost>
2012-06-13 10:05:16 +0100

    Added ability to get the relationship type through properties for Neo4j relationships.

--- src/neo4jGraph/Neo4jEdge.java
+++ src/neo4jGraph/Neo4jEdge.java
@@ -7,6 +7,11 @@ import org.neo4j.graphdb.Relationship;
 
 public class Neo4jEdge implements IEdge {
 	
+	/**
+	 * Property for different types of edges, corresponds to the Relationship type in Neo4j.
+	 */
+	public static final String EDGE_TYPE = "EdgeType";
+	
 	Relationship edge;
 	Neo4jVertex start;
 	Neo4jVertex end;
@@ -29,6 +34,12 @@ public class Neo4jEdge implements IEdge {
 
 	@Override
 	public String getProperty(String key) {
+		
+		// If the property is the type of the vertex returns the name of the relationship type.
+		if (key.equals(EDGE_TYPE)) {
+			return edge.getType().name();
+		}
+		
 		return (String) edge.getProperty(key);
 	}
 
