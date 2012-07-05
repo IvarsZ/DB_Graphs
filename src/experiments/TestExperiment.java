de600c8d5b274aa69e0fd60a02cbfe29c0cdf98e
iz2 <devnull@localhost>
2012-07-05 15:32:36 +0100

    Refactored test experiment class.

--- src/experiments/TestExperiment.java
+++ src/experiments/TestExperiment.java
@@ -2,15 +2,20 @@ package experiments;
 
 import java.util.ArrayList;
 
+import org.hamcrest.Factory;
+
 import experiments.builders.IBuilder;
 import experiments.builders.RandomTree;
 import experiments.builders.UniformRandomGraph;
 import experiments.queries.ACAQuery;
 import experiments.queries.FRNQuery;
 import experiments.queries.LCAQuery;
+import graphFactories.IFactory;
 import graphFactories.MySqlFactory;
 import graphFactories.Neo4jFactory;
+import graphInterfaces.IEdge;
 import graphInterfaces.IPersistentGraph.Direction;
+import graphInterfaces.IVertex;
 
 public class TestExperiment {
 
@@ -30,8 +35,7 @@ public class TestExperiment {
 		IBuilder builder = new RandomTree(2500, 10);
 		Experiment experiment = new Experiment("rtt_and_lca", builder);
 
-		experiment.addFactory(new MySqlFactory());
-		experiment.addFactory(new Neo4jFactory());
+		addAllFactories(experiment);
 		experiment.addSeed(14124);
 		experiment.addSeed(141242356236L);
 		experiment.addQuery(new LCAQuery(500, 1000, 1000, empty, Direction.INCOMING));
@@ -47,8 +51,7 @@ public class TestExperiment {
 		IBuilder builder = new UniformRandomGraph(25, 0.5);
 		Experiment experiment = new Experiment("urg_and_aca", builder);
 
-		experiment.addFactory(new MySqlFactory());
-		experiment.addFactory(new Neo4jFactory());
+		addAllFactories(experiment);
 		experiment.addSeed(504952);
 		experiment.addSeed(1259944358674910L);
 		experiment.addQuery(new ACAQuery(5, 10, 100, empty, Direction.INCOMING));
@@ -64,8 +67,7 @@ public class TestExperiment {
 		IBuilder builder = new RandomTree(2500, 10);
 		Experiment experiment = new Experiment("rtt_and_frn", builder);
 
-		experiment.addFactory(new MySqlFactory());
-		experiment.addFactory(new Neo4jFactory());
+		addAllFactories(experiment);
 		experiment.addSeed(14124);
 		experiment.addSeed(141242356236L);
 		experiment.addQuery(new FRNQuery(500));
@@ -81,8 +83,7 @@ public class TestExperiment {
 		IBuilder builder = new UniformRandomGraph(200, 0.3);
 		Experiment experiment = new Experiment("urg_and_fnd", builder);
 
-		experiment.addFactory(new MySqlFactory());
-		experiment.addFactory(new Neo4jFactory());
+		addAllFactories(experiment);
 		experiment.addSeed(504952);
 		experiment.addSeed(1259944358674910L);
 		experiment.addQuery(new FNDQuery(50, 15));
@@ -93,5 +94,10 @@ public class TestExperiment {
 	}
 
 	private static final ArrayList<String> empty = new ArrayList<String>();
+	
+	private static final void addAllFactories(Experiment experiment) {
+		experiment.addFactory(new Neo4jFactory());
+		experiment.addFactory(new MySqlFactory());
+	}
 
 }
